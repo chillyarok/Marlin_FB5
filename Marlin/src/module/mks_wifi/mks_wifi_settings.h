4f1fec1c9195c777c5c3b9e055e74dde5ba74489
yarok <yarok@green-ya.site>
2025-01-11 11:01:08 +0300

    modified:   Marlin/src/module/mks_wifi/mks_wifi_settings.h

--- Marlin/src/module/mks_wifi/mks_wifi_settings.h
+++ Marlin/src/module/mks_wifi/mks_wifi_settings.h
@@ -1,9 +1,9 @@
 #ifndef MKS_WIFI_SETTINGS_H
 #define MKS_WIFI_SETTINGS_H
 
-//#define MKS_WIFI_ENABLED_WIFI_CONFIG 
+#define MKS_WIFI_ENABLED_WIFI_CONFIG 
 
-#define MKS_WIFI_SSID					"network"
-#define MKS_WIFI_KEY					"password"
+#define MKS_WIFI_SSID					"yar room"
+#define MKS_WIFI_KEY					"123456789"
 
 #endif
